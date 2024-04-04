2563ecfcaab8109f009df6f8576a15209d4042ca
Judy <judyy10@nycstudents.net>
2024-04-04 13:43:50 -0400

    Update main-character.py

--- main-character.py
+++ main-character.py
@@ -1,5 +1,8 @@
-class main_character():
-    def __init__(self, name, score, ___):
+class player():
+    def __init__(self, name, score, powerup):
         self.name = name
         self.score = score
-        
+        self.powerup = powerup
+
+print("Welcome! This is to be or not to be.")
+n = input("Please enter your player name: ")
