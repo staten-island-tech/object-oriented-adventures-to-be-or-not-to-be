0078d5bce5a2598d96b1268cc6fef6d5e5496260
Judy <judyy10@nycstudents.net>
2024-05-01 13:46:01 -0400

    Update activate.py

--- activate.py
+++ activate.py
@@ -1,4 +1,50 @@
-Player = [{'Name': "Judy", 'XP': 17, 'LVL': [10]}]
+Player = [{'Name': "Judy", 'XP': 14, 'LVL': [0]}]
+
+
+class rewards():
+    def reward_1():
+        for i in Player:
+            nxp = i['XP'] + 2
+            i['XP'] = nxp
+        print(Player)
+
+
+def calc_lvl():
+    quotient = []
+    for i in Player:
+        print(i['XP'])
+        dif = int(i['XP'])/8
+
+
+        quotient.append(int(dif))
+
+
+        if dif.is_integer():
+            print(f"You are now lvl {dif}!")
+            i['LVL'] = dif
+
+
+        else:
+            print(f"You are now lvl {dif}!")
+       
+"""     
+    for i in Player:
+        i['LVL'].clear()
+        i['LVL'].append(dif) """
+
+rewards.reward_1()
+calc_lvl()
+print(Player)
+
+
+
+
+
+
+
+
+
+""" Player = [{'Name': "Judy", 'XP': 8, 'LVL': [10]}]
 
 
     
@@ -7,10 +53,7 @@ for i in Player:
     dif = int(i['XP'])/8
 
 
-    Player.append(dif)
-
-
-    if dif == ["1","2","3","4","5","6","7","8"]:
+    if dif == [1,2,3,4,5,6,7,8,9,10]:
         print(f"You are now lvl {dif}!")
         i['LVL'].append(dif)
            
@@ -20,5 +63,6 @@ for i in Player:
        
     for i in Player:
         i['LVL'].clear()
-        i['LVL'].append(1)
-        print(Player)
+        i['LVL'].append()
+        print(Player) """
+
