d6fae9a9e41706524c31061f9f6ab2bad20991cb
Jessie-Wu1717 <jessiew24@nycstudents.net>
2024-05-08 13:57:48 -0400

    l

--- data.py
+++ data.py
@@ -1,48 +1,72 @@
 import json
 import os
-class Items:
-    def ___init___(self,Name,Type,Id): 
-            self.Name = Name
-            self.Type = Type
-            self.Id = Id
-           
+## Create Class for creating new dictionaries here
+
+
+class Items():
+    def __init__(self,name, type, ID):
+        self.name = name
+        self.type = type
+        self.ID = ID
+         
     def __str__(self):
-        return f"{self.Name},{self.Type},{self.Id}"
+        return f"{self.name}, {self.type}, {self.ID}"
+
+
+
+
+
 
 with open("data.json", "r") as f:
-# Serialize the updated Python list to a JSON string
+    # Serialize the updated Python list to a JSON string
     data = json.load(f)
-##Call classes in here
-    def add_item(Name,Type,Id):
-        it = Items(Name,Type,Id)
-        data.append(it.__dict__)
+    ##Call classes in here
+
+
+    def create_item(name, type, ID):
+        new_item = Items(name, type, ID)
+        data.append(new_item.__dict__)
         for i in data:
             print(i)
 
-user = input("Enter Item information Y/N?: ").upper()
-while user in "Y":
-    Name = input("enter name: ")
-    Type = input("what type of trash it is: ")
-    Id = int(input("Id number ex 1,2,3: "))
-
-    add_item(Name,Type,Id)
-
-    user = input("Enter Item information Y/N?: ")
 
 
-    
 
-     
+
+
+user_inp = input("Do you want input a new Item? (Y/N) ").upper()
+
+
+while user_inp == "Y":
+    name = (input("Enter item name: "))
+    type = (input("Enter item type: "))
+    ID = (int(input("Enter ID (integer): ")))
+
+
+    create_item(name,type,ID)
+
+
+    user_inp = input("Do you want to input another Item? (Y/N) ").upper()
+   
+
+
+
+
+
+
 #No code needed below this line
- # Creates a new JSON file with the updated data
+# Creates a new JSON file with the updated data
 new_file = "updated.json"
 with open(new_file, "w") as f:
     # Serialize the updated Python list to a JSON string
     json_string = json.dumps(data)
 
+
     # Write the JSON string to the new JSON file
     f.write(json_string)
 
- # Overwrite the old JSON file with the new one
+
+# Overwrite the old JSON file with the new one
 os.remove("data.json")
 os.rename(new_file, "data.json")
+
