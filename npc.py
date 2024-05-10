d6b7bfd347265385540795c1d4fb6b653a7fa031
Judy <judyy10@nycstudents.net>
2024-05-10 13:54:08 -0400

    Update npc.py

--- npc.py
+++ npc.py
@@ -6,7 +6,7 @@ class NPC():
         
     def __str__(self):
         return f"{self.description}"
-
+    # class template for NPC
 class quests():
     def quest_Jack():
         print("Quest #1 - Description: Collect 3 items, Reward: 5 XP")
@@ -20,12 +20,8 @@ class quests():
         print("Description: Collect 10 items, Reward: 20 XP") 
         #description
 
-class jack():
-    def __init__(self, name, location, description):
-        name = 'Jack'
-        location = 'Shopkeeper'
-        description = "Jack is a shopkeeper whose shop is alway littered, he can give quests and if you complete them, a reward will be given such as xp."
-        super(NPC).__init__(name, location, description)
+class Jack():
+    info_Jack = NPC('Jack', 'Shop', "Jack is a shopkeeper whose shop is alway littered, he can give quests and if you complete them, a reward will be given such as xp.")
         
     def __str__(self):
         return f"{self.name}, {self.location}, {self.description}"
@@ -38,22 +34,15 @@ class jack():
             input("Returning to to main location... ")
 
 class Fiona():
-    def __init__(self, name, location, description):
-        name = "Fiona"
-        location = "Flower Shop Keeper"
-        description = "Fiona is only unlocked after level 1 and she owns a flower shop where you can drop off compostable things and will reward the player with xp if non compostable, xp will be dropped- she will ask trivia questions about different items and where they should belong."
+    info_fiona = NPC('Fiona', 'Flower Shop', "Fiona is only unlocked after level 1 and she owns a flower shop where you can drop off compostable things and will reward the player with xp if non compostable, xp will be dropped- she will ask trivia questions about different items and where they should belong.")
         # dialogue = input("Fiona: Welcome to my flower shop! Would you like to (a): Compost your items or (b): Accept a quest. (Type ‘N’ to return) >> ")
-        super(NPC).__init__(name, location, description)
 
 
 class Winstell():
-    def __init__(self, name, location, description):
-        name = "Winstell"
-        location = "Recylcing Center Owner"
-        description = "Winstell owns a recycling center and the player can drop off recyclable trash or else, xp will drop."
-        # dialogue = input("Winstell: Hello, I’m Winstell. If you’ve come to recycle, you arrived at the right place! Enter (A) if you would like to recycle items in your inventory or (B) Recieve a quest. (Type 'N' to return) >> ")
-        super(NPC).__init__(name, location, description)
+    info_Winstell = NPC('Winstell', 'Recylcing Center Owner', 'Winstell owns a recycling center and the player can drop off recyclable trash and xp will drop.')
+    # dialogue = input("Winstell: Hello, I’m Winstell. If you’ve come to recycle, you arrived at the right place! Enter (A) if you would like to recycle items in your inventory or (B) Recieve a quest. (Type 'N' to return) >> ")
 
-Jack = NPC('Jack', 'Shop', "Jack is a shopkeeper whose shop is alway littered, he can give quests and if you complete them, a reward will be given such as xp.")
-input(Jack)
-print(jack.action())
+
+input(Jack.info_Jack)
+input(Fiona.info_fiona)
+input(Winstell.info_Winstell)
