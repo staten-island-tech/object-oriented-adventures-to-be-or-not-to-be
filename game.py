6ff991dfbc6f7678764c1495969c7803620e428b
Jessie-Wu1717 <jessiew24@nycstudents.net>
2024-05-13 13:57:14 -0400

    i

--- game.py
+++ game.py
@@ -1,7 +1,32 @@
 import json
 import os
+class Level:
+    
 
-class Name:
+    def calc_lvl():
+        
+        for i in Player:
+            print(i['XP'])
+            dif = int(i['XP'])/8
+
+
+         
+
+            if dif.is_integer():
+                print(f"You are now lvl {dif}!")
+                i['LVL'].append(dif)
+           
+
+
+            else:
+                print(f"You are now lvl {dif}!")
+       
+        for i in Player:
+            i['LVL']=dif
+            print(Player)
+
+
+class Name(Level):
     def append_Name():
         def __init__(self,name):
             self.name = name
@@ -12,7 +37,7 @@ with open("player.json", "r") as f:
 # Serialize the updated Python list to a JSON string
     player = json.load(f)
 ##Call classes in here
-Player = [{'Name': Name, 'XP': 0, 'LVL': [0]}]
+Player = [{'Name': 'Name', 'XP': 19, 'LVL': [0]}]
 print("this is to be or to not be game")
 n = input("Do you wanna start the game Y/N: ").upper()      
 
@@ -20,21 +45,22 @@ n = input("Do you wanna start the game Y/N: ").upper()
 while n == "Y":
     print('Ok')
     Name = input("Please enter what you would like to be called: ")
-    Player.append(Name)
+    for i in Player:
+        i['Name'] = Name
     print("Hello,",Name,"The goal of this game is to collect littered trash and learn about the best place to drop it off. This game is interactive and a great, fun learning experience.")
-    location = input("You have 6 available locations to go to. Type the number of one of the following: 1.Jack's shop, 2.Fiona's flower shop, 3.Winstell's recycling center, 4.Park, 5.Beach, 6.House, 7.Trash can: " )
+    location = input("You have 6 available locations to go to. Type the number of one of the following: 1.Jack's shop, 2.Fiona's flower shop, 3.Winstell's recycling center, 4.Park, 5.Beach, 6.House, 7.Trash can, 8.Check stats: " )
     if location == '1':
-        print("This is Jack's Shop.")
+        input("This is Jack's Shop.")
         print("Note: You can come here for quests and to collect the items from leveling up.")
-        print(Player)
+        
     elif location == "2":
-        print("This is Fiona's flower shop.")
+        input("This is Fiona's flower shop.")
         print("Note: You can come here for information on composting and quests. You can also drop off compostable trash to fertilize her beautiful flowers.")
     elif location == "3":
-        print("This is Winstell's recycling center")
+        input("This is Winstell's recycling center")
         print("Note: ")
     elif location == "4":
-        print("This the park.")
+        input("This the park.")
         print("Note: This is a place that is usually littered. You can have a higher chance of finding trash here")
     elif location == "5":
         print("This is the Beach. you can find a wider range of trash")
@@ -43,6 +69,9 @@ while n == "Y":
         print("Note: This is your house, you can relax here")
     elif location == "7":
         print("This is the trash can.")
+    elif location == "8":
+        print(Level.calc_lvl())
+        print(f"These are your stats: {Player}")
     else:
         print("oh well then.")
 
@@ -50,15 +79,13 @@ class Level:
     
 
     def calc_lvl():
-        Player = [{'Name': "Judy", 'XP': 17, 'LVL': [10]}]
-        quotient = []
+        
         for i in Player:
             print(i['XP'])
             dif = int(i['XP'])/8
 
 
-            quotient.append(int(dif))
-
+         
 
             if dif.is_integer():
                 print(f"You are now lvl {dif}!")
--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-from main import Calculator
-Calculator.add(2,3)
+Player = [{'Name': "Judy", 'XP': 14, 'LVL': 0}]
+
+
+class rewards():
+    def reward_1():
+        for i in Player:
+            nxp = i['XP'] + 2
+            i['XP'] = nxp
+        print(Player)
+
+
+def calc_lvl():
+    for i in Player:
+        print(i['XP'])
+        dif = int(i['XP'])/8
+
+
+        if dif.is_integer():
+            print(f"You are now lvl {dif}!")
+            i['LVL'] = dif
+
+
+        else:
+            print(f"You are now lvl {dif}!")
+
+
+rewards.reward_1()
+calc_lvl()
