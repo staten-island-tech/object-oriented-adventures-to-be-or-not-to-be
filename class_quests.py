e0e5244b2301d97af17dbb5239db1436aa15810c
Judy <judyy10@nycstudents.net>
2024-05-14 13:57:34 -0400

    5/14

--- class_quests.py
+++ class_quests.py
@@ -1,3 +1,5 @@
+Player = [{'Name': "Judy", 'total_XP': 0, 'LVL': 0}]
+
 class quests():
     def quest_Jack():
         print("Quest #1 - Description: Collect 3 items, Reward: 5 XP")
@@ -12,3 +14,22 @@ class quests():
         #description
 
     #change to functions that correspond to the requirement and reward given
+
+class rewards():
+    def reward_q1():
+        for i in Player:
+            new = i['XP'] + 5
+            i['XP'] = new
+        print(Player)
+
+    def reward_q2():
+        for i in Player:
+            new = i['XP'] + 10
+            i['XP'] = new
+        print(Player)
+
+    def reward_q3():
+        for i in Player:
+            new = i['XP'] + 16
+            i['XP'] = new
+        print(Player)
