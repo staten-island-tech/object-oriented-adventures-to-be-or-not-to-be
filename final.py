91f16b56285d5952dab9ee5410edf90ede8b63b5
Judy <judyy10@nycstudents.net>
2024-06-07 13:24:20 -0400

    6.7

--- final.py
+++ final.py
@@ -49,6 +49,19 @@ def guess():
                                 i['Name'] = item
                                 i['ID'] = item_gen
                                 inventory.append(add_item)
+                                # Creates a new JSON file with the updated inventory
+                                
+                                new_file = "updated.json"
+                                with open(new_file, "w") as f:
+                                    # Serialize the updated Python list to a JSON string
+                                    json_string = json.dumps(inventory)
+
+                                    # Write the JSON string to the new JSON file
+                                    f.write(json_string)
+
+                                # Overwrite the old JSON file with the new one
+                                os.remove("inventory.json")
+                                os.rename(new_file, "inventory.json")
 
                                 ask = input("Do you want to search for trash? (Y/N) ")
         else:
