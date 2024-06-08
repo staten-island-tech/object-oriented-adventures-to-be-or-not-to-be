92c1fcc96be26c60591f7ba366e71f0383a0e45d
Jessie-Wu1717 <158211820+Jessie-Wu1717@users.noreply.github.com>
2024-06-08 14:44:52 -0400

    Update game.py

--- game.py
+++ game.py
@@ -27,19 +27,22 @@ while n == "Y":
         i['Name'] = User
         print(f"Hello, {i['Name']} The goal of this game is to collect littered trash and learn about the best place to drop it off. This game is interactive and a great, fun learning!")
     location = input("You have 6 available locations to go to. Type the number of one of the following: 1.) Jack's shop, 2.Fiona's flower shop, 3.) Winstell's recycling center, 4.) Park, 5.) Beach, 6.) House, 7.Trash can, 8.Check stats: " )
+class location_1:
     if location == '1':
         input("This is Jack's Shop.")
         print("Note: You can come here for quests and to collect the items from leveling up.")
         location = input("You have 6 available locations to go to. Type the number of one of the following: 1.) Jack's shop, 2.Fiona's flower shop, 3.) Winstell's recycling center, 4.) Park, 5.) Beach, 6.) House, 7.Trash can, 8.Check stats: " )
-    elif location == "2":
+class location_2:
+    if location == "2":
         input("This is Fiona's flower shop.")
         print("Note: You can come here for information on composting and quests. You can also drop off compostable trash to fertilize her beautiful flowers.")
-    elif location == "3":
+class location_3:
+    if location == "3":
         input("This is Winstell's recycling center")
         print("Note: Winstell will only take recyclable items, if an item that is given to him is non recyclable, XP will be dropped. Please be careful. ")
         print("Note from planet Earth: Recycling the wrong materials will cause harm to OUR environment ")
-
-    elif location == "4":
+class location_4:
+    if location == "4":
         input("This the park.")
         print("Note: This is a place that is usually littered. Please help clean our littered parks.")
         locations = input("Would you like to change locations?(Y/N) ").upper()
@@ -53,8 +56,8 @@ while n == "Y":
                     location = input("You have 6 available locations to go to. Type the number of one of the following: 1.) Jack's shop, 2.Fiona's flower shop, 3.) Winstell's recycling center, 4.) Park, 5.) Beach, 6.) House, 7.Trash can, 8.Check stats: " )
 
     
-    
-    elif location == "5":
+class location_5:
+    if location == "5":
         print("This is the Beach. you can find a wider range of trash")
         while location == "N":
             #ask player if they want to find item
@@ -71,15 +74,17 @@ while n == "Y":
         if location == "Y":
             location = input("You have 6 available locations to go to. Type the number of one of the following: 1.) Jack's shop, 2.Fiona's flower shop, 3.) Winstell's recycling center, 4.) Park, 5.) Beach, 6.) House, 7.Trash can, 8.Check stats: " )
     
-            
-    elif location == "6":
+class location_6:        
+    if location == "6":
         print("This is your house")
         print("Note: This is your house, you can relax here")
-    elif location == "7":
+class location_7:
+    if location == "7":
         print("This is the trash can.")
-    elif location == "8":
-        print(calc_lvl())
-        print(f"These are your stats: {Player}")
-else:
-    print("oh well then.")
-# player find item
+class location_8:
+        if location == "8":
+            print(calc_lvl())
+            print(f"These are your stats: {Player}")
+    else:
+        print("oh well then.")
+
